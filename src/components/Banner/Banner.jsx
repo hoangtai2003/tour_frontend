d09da8e3d687f5986566af1e3b4c86e3e06e95ca
hoangtai2003 <94899016+hoangtai2003@users.noreply.github.com>
2024-09-01 23:43:37 +0700

    Banner css

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,42 +1,49 @@
-import React from 'react'
-import Carousel from 'react-bootstrap/Carousel';
-import sliderImg1 from "../../assets/images/slider/1.png"
-import sliderImg2 from "../../assets/images/slider/2.png"
-import sliderImg3 from "../../assets/images/slider/3.png"
+import React from "react";
+import { Carousel } from "react-bootstrap";
+import sliderImg from "../../assets/images/slider/1.png";
+import sliderImg1 from "../../assets/images/slider/2.png";
+import "./banner.css"
+
 const Banner = () => {
     return (
         <>
-            <section className='slider'>
-                <Carousel variant='dark'>
+            <section className="slider">
+                <Carousel variant="dark">
                     <Carousel.Item>
-                        <img src={sliderImg1} className='d-block w-100' alt="First slide" />
+                        <img src={sliderImg} className="d-block w-100" alt="First slide" />
                         <Carousel.Caption>
-                            <div className="slider_des">
-                                <h5 className="heading">Hoàng Đức Tài</h5> <span>EXPLORE WORD</span>
-                                <p className="sub_text">
-                                    Nulla vitae elit libero, a pharetra augue mollis interdum.
-                                </p>
+                        <div className="slider_des">
+                            <h5 className="heading">
+                            JOURNEY TO <span>EXPLORE WORLD</span>
+                            </h5>
+                            <p className="sub_text">
+                            Lorem Ipsum is simply dummy text of the printing and
+                            typesetting industry. Lorem Ipsum has been the industry's
+                            standard dummy text ever since the 1500s,
+                            </p>
+                        </div>
+                        </Carousel.Caption>
+                    </Carousel.Item>
 
-                            </div>
-                        </Carousel.Caption>
-                    </Carousel.Item>
                     <Carousel.Item>
-                        <img src={sliderImg2} className='d-block w-100' alt="Second slide" />
+                        <img src={sliderImg1} className="d-block w-100" alt="First slide" />
                         <Carousel.Caption>
-                        <h3>Second slide label</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                    <Carousel.Item>
-                        <img src={sliderImg3} className='d-block w-100' alt="Third slide" />
-                        <Carousel.Caption>
+                        <div className="slider_des">
+                            <h5 className="heading">
+                            BEAUTIFUL PLACE <span>TO VISIT</span>
+                            </h5>
+                            <p className="sub_text">
+                            Lorem Ipsum is simply dummy text of the printing and
+                            typesetting industry. Lorem Ipsum has been the industry's
+                            standard dummy text ever since the 1500s,
+                            </p>
+                        </div>
                         </Carousel.Caption>
                     </Carousel.Item>
                 </Carousel>
             </section>
         </>
+    );
+};
 
-    )
-}
-
-export default Banner
+export default Banner;

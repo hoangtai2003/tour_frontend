923e53ae8878bcca8a1a2fa4369ecfc05fd59e2b
taiht <Hdt13102k3@gmail.com>
2024-11-19 20:44:01 +0700

    register guide

--- src/pages/Auth/RegisterGuide.jsx
+++ src/pages/Auth/RegisterGuide.jsx
@@ -13,6 +13,7 @@ const RegisterGuide = () => {
     const [selectedLocation, setSelectedLocation] = useState(null);
     const [locations, setLocations] = useState([])
     const { url } = useContext(StoreContext)
+    const [selectedFile, setSelectedFile] = useState(null);
     const navigate = useNavigate()
 
     const [formData, setFormData] = useState({
@@ -21,7 +22,10 @@ const RegisterGuide = () => {
         phone: "",
         role_id: 18,
         location_id: "",
-        status: "Ngừng hoạt động"
+        status: "Ngừng hoạt động",
+        user_experience: "",
+        dateBirthday: "",
+        gender: ""
     })
     const handleChange = (e) => {
         const name = e.target.name
@@ -30,7 +34,16 @@ const RegisterGuide = () => {
     } 
     const onSubmit = async (e) => {
         e.preventDefault();
-        const response = await axios.post(`${url}/auth/register`, formData);
+        const data = new FormData()
+        Object.entries(formData).forEach(([key, value]) => {
+            data.append(key, value)
+        })
+        if (selectedFile){
+            data.append("user_profile", selectedFile)
+        }
+        const response = await axios.post(`${url}/auth/register`, data, {
+            headers: { 'Content-Type': 'multipart/form-data' }
+        });
         if (response.data.success) {
             Swal.fire({
                 icon: 'success',
@@ -69,12 +82,15 @@ const RegisterGuide = () => {
         setFormData(prev => ({ ...prev, location_id: selectedOption ? selectedOption.value : "" }));
     };
     
+    const handleFileChange = (e) => {
+        setSelectedFile(e.target.files[0]);
+    };
     return (
         <div className="login-page">
             <Container>
                 <Row className='justify-content-center align-items-center'>
                     <Col lg="8" className='custom-register-row'>
-                        <div className="register-card">
+                        <div className="register-card" style={{height: "625px"}}>
                             <h2 className="register-title">Đăng ký làm hướng dẫn viên</h2>
                             <Form onSubmit={onSubmit} style={{ marginTop: "40px" }}>
                                 <div className='form-controller'>
@@ -84,7 +100,33 @@ const RegisterGuide = () => {
                                     </FormGroup>
                                     <FormGroup className='form-group'>
                                         <label className='font-bold'>Email <span>*</span></label>
-                                        <input type="email" placeholder="Email" className="login-input" name='email' required onChange={handleChange} />
+                                        <input 
+                                            type="email" 
+                                            placeholder="Email" 
+                                            className="login-input" 
+                                            name='email' 
+                                            required 
+                                            onChange={handleChange} 
+                                        />
+                                    </FormGroup>
+                                    <FormGroup className='form-group'>
+                                        <label className='font-bold'>Ngày sinh <span>*</span></label>
+                                        <input type="date" className="login-input" name='dateBirthday' required onChange={handleChange} />
+                                    </FormGroup>
+                                    <FormGroup className='form-group'>
+                                        <label className='font-bold'>Giới tính<span>*</span></label>
+                                        <select
+                                            name="gender"
+                                            className="login-input"
+                                            style={{ width: "100%" }}
+                                            value={formData.gender} 
+                                            onChange={handleChange}
+                                            required
+                                        >
+                                            <option value="" disabled>Chọn giới tính</option>
+                                            <option value="Nam">Nam</option>
+                                            <option value="Nữ">Nữ</option>
+                                        </select>
                                     </FormGroup>
                                     <FormGroup className='form-group'>
                                         <label className='font-bold'>Số điện thoại <span>*</span></label>
@@ -102,14 +144,14 @@ const RegisterGuide = () => {
                                             required
                                         />
                                     </FormGroup>
-                                    {/* <FormGroup className='form-group'>
+                                    <FormGroup className='form-group'>
                                         <label className='font-bold'>Kinh nghiệm làm việc <span>*</span></label>
                                         <input type="number" placeholder="Kinh nghiệm làm việc" className="login-input" name='user_experience' required onChange={handleChange} />
                                     </FormGroup>
                                     <FormGroup className='form-group'>
-                                        <label className='font-bold'>Curriculum Vitae (CV) <span>*</span></label>
-                                        <input type="file" accept=".pdf" className="login-input" name='user_profile' required onChange={handleChange} />
-                                    </FormGroup> */}
+                                        <label className='font-bold'>Sơ yếu lý lịch (CV) <span>*</span></label>
+                                        <input type="file" accept=".pdf" className="login-input" name='user_profile' required onChange={handleFileChange} />
+                                    </FormGroup>
                                 </div>
                                 <div className="register-card-body">
                                     <button style={{ marginTop: "60px" }} className="register-btn font-bold" type="submit">Đăng ký</button>
